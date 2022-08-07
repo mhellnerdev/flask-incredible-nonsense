5611fc94c422cb2e35b06af2ec1617b3641b3242
mhellnerdev <mhellnerdev@gmail.com>
2022-08-07 17:13:24 -0400

    add prepositions to put between random words

---
+++ prepositions.py
@@ -0,0 +1,91 @@
+# prepositions.py
+
+import random
+
+
+prepositions = [
+  "aboard",
+  "about",
+  "above",
+  "across",
+  "after",
+  "against,"
+  "along",
+  "amid",
+  "among",
+  "anti",
+  "around,"
+  "as",
+  "at",
+  "before",
+  "behind",
+  "below",
+  "beneath",
+  "beside",
+  "besides",
+  "between",
+  "beyond",
+  "but",
+  "by",
+  "concerning",
+  "considering",
+  "despite",
+  "down",
+  "during",
+  "except",
+  "excepting",
+  "excluding",
+  "following",
+  "for",
+  "from",
+  "in",
+  "inside",
+  "into",
+  "like",
+  "minus",
+  "near",
+  "of",
+  "off",
+  "on",
+  "onto",
+  "opposite",
+  "outside",
+  "over",
+  "past",
+  "per",
+  "plus",
+  "regarding",
+  "round",
+  "save",
+  "since",
+  "than",
+  "through",
+  "to",
+  "toward",
+  "towards",
+  "under",
+  "underneath",
+  "unlike",
+  "until",
+  "up",
+  "upon",
+  "versus",
+  "via",
+  "with",
+  "within",
+  "without",
+]
+
+
+
+def random_preposition():
+  random_preposition = (random.choice(prepositions))
+  return random_preposition
+
+
+
+
+# if name == main
+if __name__ == "__main__":
+  random_preposition()
+
