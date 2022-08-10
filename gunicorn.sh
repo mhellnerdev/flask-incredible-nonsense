bf16bb40506dcfc4d09c2fd0cb214a7b8a90e6d9
mhellnerdev <mhellnerdev@gmail.com>
2022-08-10 04:22:21 +0000

    add logging to gunicorn command

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-gunicorn --bind 0.0.0.0:5000 -w 3 app:app
+mkdir -p ./log/ && touch $_/gunicorn-access.log
+
+gunicorn --bind 0.0.0.0:5000 -w 3 --access-logfile ./log/gunicorn-access.log app:app
