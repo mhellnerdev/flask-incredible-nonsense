21b095c152dfd1c13b627044981682318a75e3b5
mhellnerdev <mhellnerdev@gmail.com>
2022-08-12 04:52:02 +0000

    random img test

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,11 +1,21 @@
+var myImg = new Array("/static/img/loading-hamster.gif", "/static/img/loading-rube.gif", "/static/img/loading-cat-01.gif")
+
+// window.onload = chooseImg
+
+window.addEventListener('DOMContentLoaded', (event) => {
+  chooseImg();
+})
+
+
 // <![CDATA[
 function loading() {
-    $("#loading").show();
-    $("#content").hide();
-    $("#footer").hide();
+  $("#loading").show();
+  $("#content").hide();
+  $("#footer").hide();
 }
-  // ]]>
+// ]]>
 
-  window.onload = chooseImgl
-
-  var myImg = imgArray{""}
+function chooseImg() {
+  var randomNum = Math.floor(Math.random() * myImg.length);
+  document.getElementById("loading-img").src = myImg[randomNum];
+}
