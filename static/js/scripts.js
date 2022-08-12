7da9aab90a2ee94811791aaa6004828aac1cd56c
mhellnerdev <mhellnerdev@gmail.com>
2022-08-12 04:58:03 +0000

    random img js working

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,7 +1,5 @@
 var myImg = new Array("/static/img/loading-hamster.gif", "/static/img/loading-rube.gif", "/static/img/loading-cat-01.gif")
 
-// window.onload = chooseImg
-
 window.addEventListener('DOMContentLoaded', (event) => {
   chooseImg();
 })
