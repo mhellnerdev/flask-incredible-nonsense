884990a1622b2ccf1c5400da32ce24df0e65e9de
mhellnerdev <mhellnerdev@gmail.com>
2022-08-13 01:03:06 +0000

    add clean dangling on rebuild

--- build_run.sh
+++ build_run.sh
@@ -4,6 +4,8 @@ docker rm incrediblenonsense -f
 
 docker rmi incrediblenonsense -f
 
+docker rmi $(docker images -f "dangling=true" -q)
+
 docker image build -t incrediblenonsense .
 
 docker run --name "incrediblenonsense" -d -p 5000:5000 incrediblenonsense
