fe8808fd4f4b04fad9397fc0a4f40077a208942b
mhellnerdev <mhellnerdev@gmail.com>
2022-08-13 01:03:55 +0000

    localize variables to function

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,19 +1,16 @@
-var myImg = new Array("/static/img/loading-hamster.gif", "/static/img/loading-cat-01.gif")
-
 window.addEventListener('DOMContentLoaded', (event) => {
   chooseImg();
 })
 
 
-// <![CDATA[
 function loading() {
   $("#loading").show();
   $("#content").hide();
   $("#footer").hide();
 }
-// ]]>
 
 function chooseImg() {
+  var myImg = new Array("/static/img/loading-hamster.gif", "/static/img/loading-cat-01.gif")
   var randomNum = Math.floor(Math.random() * myImg.length);
   document.getElementById("loading-img").src = myImg[randomNum];
 }
