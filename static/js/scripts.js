f78dd0db56d27c2e62413645d79b041727c3150e
mhellnerdev <mhellnerdev@gmail.com>
2022-09-04 19:32:34 +0000

    removed imag

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -10,7 +10,7 @@ function loading() {
 }
 
 function chooseImg() {
-  var myImg = new Array("/static/img/loading-hamster.gif", "/static/img/loading-cat-01.gif", "/static/img/loading-cat-02.gif", "/static/img/loading-dog-01.gif", "/static/img/loading-doge.gif", "/static/img/loading-kermit.gif", "/static/img/loading-typing-01.gif")
+  var myImg = new Array("/static/img/loading-hamster.gif", "/static/img/loading-cat-01.gif", "/static/img/loading-cat-02.gif", "/static/img/loading-dog-01.gif", "/static/img/loading-doge.gif", "/static/img/loading-kermit.gif")
   var randomNum = Math.floor(Math.random() * myImg.length);
   document.getElementById("loading-img").src = myImg[randomNum];
 }
